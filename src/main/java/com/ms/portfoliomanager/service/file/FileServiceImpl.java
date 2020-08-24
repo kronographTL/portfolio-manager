795c30402cd8f619c33b64ca33f67f3052da6b56
kronographTL <kronographtl@gmail.com>
2020-08-25 01:25:59 +0800

    Extracting the creation of comma separation of data in a method for writing to Excel file

--- src/main/java/com/ms/portfoliomanager/service/file/FileServiceImpl.java
+++ src/main/java/com/ms/portfoliomanager/service/file/FileServiceImpl.java
@@ -1,19 +1,15 @@
 package com.ms.portfoliomanager.service.file;
 
-import com.ms.portfoliomanager.domainValue.PositionType;
-import com.ms.portfoliomanager.model.CallPosition;
 import com.ms.portfoliomanager.model.Portfolio;
-import com.ms.portfoliomanager.model.PutPosition;
-import com.ms.portfoliomanager.model.StockPosition;
 import com.ms.portfoliomanager.util.CommonUtil;
 import com.ms.portfoliomanager.util.ExportToExcelUtil;
+import com.ms.portfoliomanager.util.PositionUtil;
 import com.ms.portfoliomanager.util.UtilityConstants;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 public class FileServiceImpl implements FileService{
@@ -45,46 +41,15 @@ public class FileServiceImpl implements FileService{
         String dataHeader = UtilityConstants.DATA_HEADER;
         StringBuilder dataBody = new StringBuilder();
         if(portfolio!=null){
-            List<StockPosition> stockPositions = portfolio.getStockPositions();
-            if (stockPositions !=null){
-                for (StockPosition pos : stockPositions){
-                    dataBody.append(PositionType.STOCK).append(UtilityConstants.COMMA)
-                            .append(pos.getShareCode()).append(UtilityConstants.COMMA)
-                            .append(pos.getShareName()).append(UtilityConstants.COMMA)
-                            .append(pos.getNoOfShares()).append(UtilityConstants.COMMA)
-                            .append(pos.getCurrentValue()).append(UtilityConstants.COMMA)
-                            .append(pos.getTotalValue()).append(UtilityConstants.COMMA)
-                            .append(System.lineSeparator());
-                }
+            if (portfolio.getStockPositions() !=null){
+                portfolio.getStockPositions().forEach(position -> PositionUtil.getStockPositionElementsWithComaSeparation(dataBody,position));
+            }
+            if (portfolio.getCallPositions() !=null){
+                portfolio.getCallPositions().forEach(position -> PositionUtil.getCallPositionElementsWithComaSeparation(dataBody,position));
 
             }
-
-            List<CallPosition> callPositions = portfolio.getCallPositions();
-            if (stockPositions !=null){
-                for (CallPosition pos : callPositions){
-                    dataBody.append(PositionType.CALL).append(UtilityConstants.COMMA)
-                            .append(pos.getShareCode()).append(UtilityConstants.COMMA)
-                            .append(pos.getShareName()).append(UtilityConstants.COMMA)
-                            .append(pos.getNoOfShares()).append(UtilityConstants.COMMA)
-                            .append(pos.getTheoreticalValue()).append(UtilityConstants.COMMA)
-                            .append(pos.getTotalValue()).append(UtilityConstants.COMMA)
-                            .append(System.lineSeparator());
-                }
-
-            }
-
-            List<PutPosition> putPositions = portfolio.getPutPositions();
-            if (putPositions !=null){
-                for (PutPosition pos : putPositions){
-                    dataBody.append(PositionType.PUT).append(UtilityConstants.COMMA)
-                            .append(pos.getShareCode()).append(UtilityConstants.COMMA)
-                            .append(pos.getShareName()).append(UtilityConstants.COMMA)
-                            .append(pos.getNoOfShares()).append(UtilityConstants.COMMA)
-                            .append(pos.getTheoreticalValue()).append(UtilityConstants.COMMA)
-                            .append(pos.getTotalValue()).append(UtilityConstants.COMMA)
-                            .append(System.lineSeparator());
-                }
-
+            if (portfolio.getPutPositions() !=null){
+                portfolio.getPutPositions().forEach(position -> PositionUtil.getPutPositionElementsWithComaSeparation(dataBody,position));
             }
             dataBody.append(System.lineSeparator())
                     .append(UtilityConstants.COMMA).append(UtilityConstants.COMMA).append(UtilityConstants.COMMA).append(UtilityConstants.COMMA)
@@ -94,4 +59,5 @@ public class FileServiceImpl implements FileService{
         String fileName = portfolio.getUserName().toUpperCase()+fileNamePrefix + CommonUtil.getDateString(LocalDateTime.now(),"ddMMMyy")+".xlsx";
         ExportToExcelUtil.exportExcel(response,dataHeader,dataBody.toString(),fileName, UtilityConstants.COMMA);
     }
+
 }
---
+++ src/main/java/com/ms/portfoliomanager/util/PositionUtil.java
@@ -0,0 +1,37 @@
+package com.ms.portfoliomanager.util;
+
+import com.ms.portfoliomanager.domainValue.PositionType;
+import com.ms.portfoliomanager.model.CallPosition;
+import com.ms.portfoliomanager.model.PutPosition;
+import com.ms.portfoliomanager.model.StockPosition;
+
+public class PositionUtil {
+
+    public static void getStockPositionElementsWithComaSeparation(StringBuilder dataBody, StockPosition position) {
+        dataBody.append(PositionType.STOCK).append(UtilityConstants.COMMA)
+                .append(position.getShareCode()).append(UtilityConstants.COMMA)
+                .append(position.getShareName()).append(UtilityConstants.COMMA)
+                .append(position.getNoOfShares()).append(UtilityConstants.COMMA)
+                .append(position.getCurrentValue()).append(UtilityConstants.COMMA)
+                .append(position.getTotalValue()).append(UtilityConstants.COMMA)
+                .append(System.lineSeparator());
+    }
+    public static void getCallPositionElementsWithComaSeparation(StringBuilder dataBody, CallPosition position) {
+        dataBody.append(PositionType.STOCK).append(UtilityConstants.COMMA)
+                .append(position.getShareCode()).append(UtilityConstants.COMMA)
+                .append(position.getShareName()).append(UtilityConstants.COMMA)
+                .append(position.getNoOfShares()).append(UtilityConstants.COMMA)
+                .append(position.getTheoreticalValue()).append(UtilityConstants.COMMA)
+                .append(position.getTotalValue()).append(UtilityConstants.COMMA)
+                .append(System.lineSeparator());
+    }
+    public static void getPutPositionElementsWithComaSeparation(StringBuilder dataBody, PutPosition position) {
+        dataBody.append(PositionType.STOCK).append(UtilityConstants.COMMA)
+                .append(position.getShareCode()).append(UtilityConstants.COMMA)
+                .append(position.getShareName()).append(UtilityConstants.COMMA)
+                .append(position.getNoOfShares()).append(UtilityConstants.COMMA)
+                .append(position.getTheoreticalValue()).append(UtilityConstants.COMMA)
+                .append(position.getTotalValue()).append(UtilityConstants.COMMA)
+                .append(System.lineSeparator());
+    }
+}
